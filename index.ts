9d4ea69ad9c1cea64589143e59764c8f20ca37b9
himu <satirtharoy2003@gmail.com>
2024-01-25 17:28:14 +0600

    types

---
+++ index.ts
@@ -0,0 +1,20 @@
+const head = document.querySelector('h1');
+console.log(head);
+
+const area = (d: number)=> d*Math.PI;
+
+let d:number = 12;
+
+let obj: {
+  name: string,
+  age: number,
+  belt: string
+}
+
+obj = {
+  name: 'satirtha',
+  age: 21,
+  belt: 'black'
+}
+
+console.log(obj);
