e4303924844fdf68e20605bba7c550f92e07b795
himu <satirtharoy2003@gmail.com>
2024-01-26 16:02:31 +0600

    type aliases

--- public/index.js
+++ public/index.js
@@ -1,46 +1,11 @@
 "use strict";
-const head = document.querySelector('h1');
-console.log(head);
-// function parameter type is set like this
-const area = (d) => d * Math.PI;
-let d = 12;
-// obj
-let obj;
-obj = {
-    name: 'satirtha',
-    age: 21,
-    belt: 'black'
+// type aliases
+// a function
+const greeting = (name, id) => {
+    console.log(`hellow ${name}.. your id is ${id}`);
 };
-console.log(obj);
-// another way of setting obj types
-let anObj;
-anObj = [1, 2, 3]; // because an array is an object
-// array types
-let arr; // we cannot push items in this but can only declare in
-let arr1 = [];
-arr1.push('himu', 'satirtha', 'roy'); // this way we can push items in an arra
-// union types
-let mixedArr = [];
-mixedArr = [12, 'himu', false, 'black'];
-console.log("mixed arr = " + mixedArr);
-let value;
-value = 'name';
-value = 21;
-value = false; // it can bbe str num or bool but cannot be object or array
-let anotherValue; // this can be of any type
-anotherValue = false;
-anotherValue = 12;
-anotherValue = [1, 2, 'satirtha'];
-anotherValue = {
-    name: 'satirtha',
-    age: 21,
-    isMarried: false
+const greet = (info) => {
+    const { name, uid } = info; // destucturing obj
+    console.log(`hellow ${name}. your id ${uid}`);
 };
-// function is also a value
-anotherValue = (param) => {
-    console.log(param);
-};
-const sum = (a, b, c) => a + b; //c is optional its default value is 0
-const summation = (a, b, c = 10) => a + b + c;
-let result = sum(10, 20);
-let sumResult = summation(11, 31);
+greet({ name: 'satirtha', uid: 1234 });
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,17 @@
-const head = document.querySelector('h1');
-console.log(head);
-// function parameter type is set like this
-const area = (d: number)=> d*Math.PI;
+// type aliases
 
-let d:number = 12;
+type strOrnum = string | number; //by type aliases we can declare our own custom types and what the type shall include
+type objName = {name: string, uid: strOrnum};
 
-// obj
-let obj: { //has to be a colone not equal sign cause we are declaring the data types of the obj not the value
-  name: string,
-  age: number,
-  belt: string
+// a function
+
+const greeting = (name: string, id: string|number) => { // we can use type aliases if we use the same type multiple times
+  console.log(`hellow ${name}.. your id is ${id}`);
 }
 
-obj = {
-  name: 'satirtha',
-  age: 21,
-  belt: 'black'
+const greet = (info: objName) => {
+  const {name,uid} = info; // destucturing obj
+  console.log(`hellow ${name}. your id ${uid}`);
 }
 
-console.log(obj);
-
-// another way of setting obj types
-let anObj: object;
-anObj = [1,2,3]; // because an array is an object
-
-// array types
-
-let arr: string[]; // we cannot push items in this but can only declare in
-
-let arr1: string[] =[];
-
-arr1.push('himu', 'satirtha', 'roy'); // this way we can push items in an arra
-
-// union types
-
-let mixedArr: (string|number|boolean)[] = [];
-mixedArr = [12, 'himu', false, 'black'];
-console.log("mixed arr = " + mixedArr)
-
-let value: string|number|boolean;
-
-value = 'name';
-value = 21;
-value = false; // it can bbe str num or bool but cannot be object or array
-
-let anotherValue: any; // this can be of any type
-
-anotherValue = false;
-anotherValue = 12;
-anotherValue = [1,2, 'satirtha'];
-anotherValue = {
-  name: 'satirtha',
-  age: 21,
-  isMarried: false
-}
-
-// function is also a value
-anotherValue = (param:any)=> {
-  console.log(param);
-}
-
-
-const sum = (a:number, b:number, c?:number|string) => a+b; //c is optional its default value is 0
-
-const summation = (a:number, b:number, c:number = 10) => a+b+c;
-
-let result = sum(10,20);
-let sumResult = summation(11,31);
+greet({name: 'satirtha', uid: 1234});
